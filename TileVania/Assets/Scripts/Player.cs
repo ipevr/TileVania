8911aa27d2860b8303b1b9d88b77c5723b2a7a77
Ingo Peters <ingo.peters@iav.de>
2018-09-09 19:15:00 +0200

    - layer touch works - climbing ladders implemented

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -5,25 +5,25 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField] float runSpeed = 1f;
-    [SerializeField] float jumpVerticalPower = 5f;
-    [SerializeField] float jumpHorizontalPower = 2f;
+    [SerializeField] float runSpeed = 3f;
+    [SerializeField] float climbSpeedHorizontal = 1f;
+    [SerializeField] float climbSpeedVertical = 1f;
+    [SerializeField] float jumpVerticalPower = 7f;
+    [SerializeField] float jumpHorizontalPower = 1.3f;
 
     bool jumping = false;
 
     Rigidbody2D myRigidbody;
     Collider2D myCollider;
     Animator myAnimator;
-    string[] jumpLayerNames = new string[1];
-    int jumpLayerMask;
+    float gravityScaleAtStart = 0f;
 
     // Use this for initialization
     void Start () {
-        jumpLayerNames[0] = "Ground";
         myRigidbody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<Collider2D>();
         myAnimator = GetComponent<Animator>();
-        jumpLayerMask = LayerMask.GetMask(jumpLayerNames);
+        gravityScaleAtStart = myRigidbody.gravityScale;
 	}
 	
 	// Update is called once per frame
@@ -32,21 +32,42 @@ public class Player : MonoBehaviour {
             Run();
         }
         Jump();
+        ClimbLadder();
     }
 
     private void Jump() {
-        if (!myCollider.IsTouchingLayers(jumpLayerMask)) {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"))) {
+            Debug.Log("Jump on");
             jumping = true;
             return;
         }
         if (CrossPlatformInputManager.GetButtonDown("Jump") && !jumping) {
+            Debug.Log("Jump");
+            gameObject.transform.position += new Vector3(0f, 0.01f, 0f); // give player a small vertical shift to prevent switching jumping to false in the next frame because player is still touching ground layer
             Vector2 jumpVelocity = new Vector2(jumpHorizontalPower * Mathf.Sign(myRigidbody.velocity.x), jumpVerticalPower);
             myRigidbody.velocity = jumpVelocity;
+            jumping = true;
             return;
         }
-        if (myCollider.IsTouchingLayers(jumpLayerMask)) {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"))) {
+            Debug.Log("Jump off");
             jumping = false;
         }
+    }
+
+    private void ClimbLadder() {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+            myAnimator.SetBool("Climbing", false);
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+        myRigidbody.gravityScale = 0f;
+        float controlThrowY = CrossPlatformInputManager.GetAxis("Vertical");
+        float controlThrowX = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrowX * climbSpeedHorizontal, controlThrowY * climbSpeedVertical);
+        myRigidbody.velocity = playerVelocity;
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
     }
 
     private void Run() {
